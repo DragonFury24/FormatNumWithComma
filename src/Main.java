70fd446e17591bce3bd33a4d8cc42996bb067a3e
Dragonfury24 <dennisdnguyen19@gmail.com>
2018-05-23 20:54:37 -0400

    Format without String manipulation

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         ArrayList<Long> inputs = FileUtils.readFile(new File(new File("src").exists() ? "src/SampleInput.txt" : "SampleInput.txt"));
 
         inputs.forEach(input -> System.out.println(formatWithComma(Long.toString(input))));
-
+        inputs.forEach(input -> System.out.println(formatWithComma(input)));
     }
 
     /**
@@ -21,5 +21,22 @@ public class Main {
         return formatWithComma(input.substring(0, input.length() - 3)) + "," + input.substring(input.length() - 3);
     }
 
+    public static String formatWithComma(long input) {
+        long mod = input % 1000;
+        long div = input / 1000;
 
+        if (div == 0)
+            return Long.toString(mod);
+
+        if (input <= 100)
+            return Long.toString(input);
+
+        if (mod < 10)
+            return formatWithComma(div) + "," + "00" + Long.toString(mod);
+
+        if (mod < 100)
+            return formatWithComma(div + "," + "0" + Long.toString(mod);
+
+        return formatWithComma(div) + "," + Long.toString(mod);
+    }
 }
